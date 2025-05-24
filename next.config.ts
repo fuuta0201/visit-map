96d16111d6bd1aa0227be740f92f97f2661e05bc
fuuta0201 <satofuta0201@gmail.com>
2025-05-24 19:57:27 +0900

    fix: import alias

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname, 'src'),
-    };
-    return config;
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        '@': './src',
+      },
+    },
   },
 };
 
