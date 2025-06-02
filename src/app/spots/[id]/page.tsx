ebd0b71c9ab168ce0604397fef96abde1893a4b6
fuuta0201 <satofuta0201@gmail.com>
2025-06-02 23:08:57 +0900

    add: spot detail page template

---
+++ src/app/spots/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="pt-24 pb-16 md:pt-32 md:pb-24">
+      <div className="container mx-auto px-4 lg:px-20"></div>
+    </div>
+  );
+};
+
+export default page;
