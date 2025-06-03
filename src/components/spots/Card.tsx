554b1772bc1f428daa46e7f2372d69ba5bfb4a9d
fuuta0201 <satofuta0201@gmail.com>
2025-06-03 17:55:54 +0900

    fix: spots card component

--- src/components/spots/Card.tsx
+++ src/components/spots/Card.tsx
@@ -3,24 +3,36 @@ import Image from 'next/image';
 
 type Props = {
   title: string;
-  body: string;
   addr: string;
+  userName: string;
   figure: string;
 };
 
-const Card = ({ title, body, addr, figure }: Props) => {
+const Card = ({ title, addr, userName, figure }: Props) => {
   return (
-    <div className="w-full flex flex-col-reverse md:flex-row gap-4">
+    <div className="w-full flex flex-col-reverse md:flex-row md:justify-between gap-4">
       <div>
         <h2 className="text-xl lg:text-2xl font-bold mb-2">{title}</h2>
-        <p className="text-sm font-normal mb-1">{body}</p>
-        <dl className="flex items-center gap-2 text-sm text-gray-500">
-          <dt>所在地：</dt>
-          <dd>{addr}</dd>
+        <dl className="text-sm text-gray-500">
+          <div className="flex items-center gap-2">
+            <dt>所在地：</dt>
+            <dd>{addr}</dd>
+          </div>
+          <div className="flex items-center gap-2">
+            <dt>投稿者：</dt>
+            <dd>{userName}</dd>
+          </div>
         </dl>
       </div>
       <figure className="w-full md:w-80">
-        <Image src={figure} className="rounded-md" width={320} height={170} alt="" />
+        <Image
+          src={figure}
+          className="rounded-md w-full h-auto"
+          width={320}
+          height={170}
+          sizes="100%"
+          alt=""
+        />
       </figure>
     </div>
   );
