7172ffb35ebd9462dff5f710bfd851ab28f71d8d
fuuta0201 <satofuta0201@gmail.com>
2025-06-03 18:33:38 +0900

    feat: spotsのデータサーバー取得

--- src/app/spots/page.tsx
+++ src/app/spots/page.tsx
@@ -1,60 +1,20 @@
-'use client';
-import React, { ChangeEvent, useState } from 'react';
+import React from 'react';
+import { getSpots } from '@/lib/dummyData';
 
 // UI
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import Card from '@/components/spots/Card';
+import SpotsDisplay from '@/components/spots/SpotsDisplay';
 
-const page = () => {
-  const [sortType, setSortType] = useState<string>('post');
+const page = async () => {
+  const spots = await getSpots();
+
+  if (!spots) {
+    return;
+  }
 
   return (
     <div className="pt-24 pb-16 md:pt-32 md:pb-24">
       <div className="container mx-auto px-4 lg:px-20">
-        <ul className="flex flex-col md:grid md:grid-cols-3 gap-3 w-full md:w-96 mb-10">
-          <li className="w-full flex flex-col justify-start gap-2">
-            <Label htmlFor="prefecture">都道府県</Label>
-            <Input name="prefecture" placeholder="東京都" />
-          </li>
-          <li className="w-full flex flex-col justify-start gap-2">
-            <Label htmlFor="city">市区町村</Label>
-            <Input name="city" placeholder="新宿区" />
-          </li>
-          <li>
-            <Label htmlFor="sort">並び替え</Label>
-            <Select name="sort" defaultValue="post" onValueChange={(type) => setSortType(type)}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="post">投稿日順</SelectItem>
-                <SelectItem value="eval">評価順</SelectItem>
-              </SelectContent>
-            </Select>
-          </li>
-        </ul>
-        <ul className="flex flex-col gap-7">
-          {Array(10)
-            .fill(0)
-            .map((_, index) => (
-              <li key={index}>
-                <Card
-                  title="長岡高専"
-                  body="ニキたちの集まりです。ニキたちの集まりです。ニキたちの集まりです。ニキたちの集まりです。ニキたちの集まりです。ニキたちの集まりです。ニキたちの集まりです。"
-                  addr="新潟県長岡市西片貝町888"
-                  figure="/images/dummy-image.png"
-                />
-              </li>
-            ))}
-        </ul>
+        <SpotsDisplay spotsProp={spots} />
       </div>
     </div>
   );
---
+++ src/components/spots/SpotsDisplay.tsx
@@ -0,0 +1,92 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import type { Spot } from '@/lib/dummyData';
+import Link from 'next/link';
+
+// UI
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import Card from '@/components/spots/Card';
+
+type Props = {
+  spotsProp: Spot[];
+};
+
+const SpotsDisplay = ({ spotsProp }: Props) => {
+  const [spots, setSpots] = useState<Spot[] | undefined>([]);
+
+  useEffect(() => {
+    if (spotsProp) {
+      setSpots({ ...spotsProp });
+    }
+  }, []);
+
+  if (!spots) {
+    return;
+  }
+
+  const handleSortChange = (type: string) => {
+    let sortedSpots: Spot[] = spots;
+
+    if (type === 'post') {
+      sortedSpots = spots.sort((a: Spot, b: Spot) => {
+        return Date.parse(b.createdAt) - Date.parse(a.createdAt);
+      });
+    } else if (type === 'rate') {
+      sortedSpots = spots.sort((a: Spot, b: Spot) => {
+        return b.rating - a.rating;
+      });
+    }
+
+    setSpots({ ...sortedSpots });
+  };
+  return (
+    <>
+      <ul className="flex flex-col md:grid md:grid-cols-3 gap-3 w-full md:w-96 mb-10">
+        <li className="w-full flex flex-col justify-start gap-2">
+          <Label htmlFor="prefecture">都道府県</Label>
+          <Input name="prefecture" placeholder="東京都" />
+        </li>
+        <li className="w-full flex flex-col justify-start gap-2">
+          <Label htmlFor="city">市区町村</Label>
+          <Input name="city" placeholder="新宿区" />
+        </li>
+        <li>
+          <Label htmlFor="sort">並び替え</Label>
+          <Select name="sort" defaultValue="post" onValueChange={(type) => handleSortChange(type)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="post">投稿日順</SelectItem>
+              <SelectItem value="rate">評価順</SelectItem>
+            </SelectContent>
+          </Select>
+        </li>
+      </ul>
+      <ul className="flex flex-col gap-7">
+        {spots.map((spot, _) => (
+          <li key={spot.id}>
+            <Link href={`/spots/${spot.id}`}>
+              <Card
+                title={spot.title}
+                addr={`${spot.prefecture}${spot.city}`}
+                userName={spot.userName}
+                figure={spot.imageUrl}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default SpotsDisplay;
